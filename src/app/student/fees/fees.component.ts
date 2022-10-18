4bdc374c973889f286d0f4e08e530928e9d79894
Riyaz <riyazahmedangrio@gmail.com>
2022-10-18 14:06:10 +0530

    Update fees.component.ts

--- src/app/student/fees/fees.component.ts
+++ src/app/student/fees/fees.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fees',
   templateUrl: './fees.component.html',
-  styleUrls: ['./fees.component.scss']
+  styleUrls: ['./fees.component.scss'],
 })
 export class FeesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
